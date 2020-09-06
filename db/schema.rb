770e01c686e7d28e75e613172a7972583ef6593b
mdm106 <mads.mcmahon@googlemail.com>
2020-09-06 09:13:11 +0100

    Schema updated to testing rollbacks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_06_075207) do
+ActiveRecord::Schema.define(version: 2020_09_06_080203) do
 
   create_table "owners", force: :cascade do |t|
     t.string "first_name", limit: 50
