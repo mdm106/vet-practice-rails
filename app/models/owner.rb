98aae123079de1b2105bc7cb0dcc7f8cf310419f
mdm106 <mads.mcmahon@googlemail.com>
2021-02-28 09:35:08 +0000

    Adding formtting owner methods

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,2 +1,15 @@
 class Owner < ApplicationRecord
+
+    def full_name
+      "#{self.first_name} #{self.last_name}"
+    end
+
+    def full_address
+      "#{self.address_1}, #{self.address_2}, #{self.town}, #{self.postcode}"
+    end
+
+    def format_telephone
+      "#{self.telephone[0..3]} #{self.telephone[4..]}"
+    end
+
 end
