5cc3fde90edea7d6b071bab8380a10e235984df3
mdm106 <mads.mcmahon@googlemail.com>
2021-02-28 12:02:39 +0000

    Adding greeting to welcome index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,19 @@
 class WelcomeController < ApplicationController
 
     def index
-      @owners = Owner.all
+        t = Time.now()
+        currentHour = t.hour
+        @greeting = "";
+        if currentHour < 12
+            @greeting="Good Morning";
+        elsif currentHour < 17
+            @greeting="Good Afternoon";
+        elsif currentHour < 22
+            @greeting="Good Evening";
+        else
+            @greeting="Good Night";
+        end
+        
     end
 
 end
