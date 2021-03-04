5177c2ba69e8163f75a3e5c6f6b1063831a0f12c
mdm106 <mads.mcmahon@googlemail.com>
2021-03-04 21:17:09 +0000

    Creating user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_28_085156) do
+ActiveRecord::Schema.define(version: 2021_03_04_211108) do
 
   create_table "owners", force: :cascade do |t|
     t.string "first_name"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 2021_02_28_085156) do
     t.string "address_2"
     t.string "town", limit: 50
     t.string "postcode", limit: 8
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
