4eb260126cf164704fa902869a4cf02db053a715
mdm106 <mads.mcmahon@googlemail.com>
2021-03-07 16:26:53 +0000

    Adding create and destroy sessions methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,22 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(email: params[:email])
+   if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to '/welcome'
+   else
+      redirect_to '/login'
+   end
   end
 
   def login
+  end
+
+  def destroy
+    session.delete(:user_id)
+    @current_user = nil
+    redirect_to '/'
   end
 
   def welcome
