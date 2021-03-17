017afe288e805d060cbb18e2e54b052612422cdc
mdm106 <mads.mcmahon@googlemail.com>
2021-03-17 11:28:02 +0000

    Making log in required for all owners actions

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -2,7 +2,7 @@ class OwnersController < ApplicationController
 
     load_and_authorize_resource
 
-    before_action :require_logged_in_user, only: [:new, :create, :edit, :update]
+    before_action :require_logged_in_user
 
     def index
       @owners = Owner.paginate(page: params[:page], per_page: 10)
